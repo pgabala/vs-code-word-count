fba1021f459ef6ad3a54b3034583ac5f0c620770
pgabala <pgabala@bgsu.edu>
2017-10-29 16:28:51 -0400

    added an event listenter to handle updating the word count

--- word-count/src/extension.ts
+++ word-count/src/extension.ts
@@ -84,6 +84,31 @@ class WordCounter {
 
 }
 
+class WordCounterController {
+    private _wordCounter: WordCounter;
+    private _disposable: Disposable;
+
+    constructor(wordCounter: WordCounter) {
+        this._wordCounter = wordCounter;
+
+        let subscriptions: Disposable[] = [];
+        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
+        window.onDidChangeActiveTextEditor(this._onEvent, htis, subscriptions);
+
+        this._wordCounter.updateWordCount();
+
+        this._disposable = Disposable.from(...subscriptions);
+    }
+    
+    dispose() {
+        this._disposable.dispose();
+    }
+
+    private _onEvent() {
+        this._wordCounter.updateWordCount();
+    }
+}
+
 // this method is called when your extension is deactivated
 export function deactivate() {
 }
